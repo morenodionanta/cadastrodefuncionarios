b011d7500037f66c16e0c8eb6eb30b167a48d3dd
alexdionanta <128551228+morenodionanta@users.noreply.github.com>
2023-06-26 20:59:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cadastrodefuncionarios
+ progeto integrador
---
+++ src/main/java/com/mycompany/cadastrodefuncionarios/Cadastrodefuncionarios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastrodefuncionarios;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cadastrodefuncionarios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
